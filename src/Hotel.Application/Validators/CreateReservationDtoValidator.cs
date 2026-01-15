c6da161f1b43cf61eca16ff35376e4d33ac6c62f
Elleass <mic.kalinowski@protonmail.com>
2026-01-15 16:48:19 +0100

    feat:  automatic validation for all DTOs

---
+++ src/Hotel.Application/Validators/CreateReservationDtoValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using Hotel.Application.DTOs;
+
+namespace Hotel.Application.Validators;
+
+public class CreateReservationDtoValidator : AbstractValidator<CreateReservationDto>
+{
+    public CreateReservationDtoValidator()
+    {
+        RuleFor(x => x.GuestId)
+            .GreaterThan(0)
+            .WithMessage("Guest ID is required");
+
+        RuleFor(x => x.RoomId)
+            .GreaterThan(0)
+            .WithMessage("Room ID is required");
+
+        RuleFor(x => x.StartDate)
+            .NotEmpty()
+            .WithMessage("Start date is required")
+            .GreaterThanOrEqualTo(DateTime.Today)
+            .WithMessage("Start date cannot be in the past");
+
+        RuleFor(x => x.EndDate)
+            .NotEmpty()
+            .WithMessage("End date is required")
+            .GreaterThan(x => x.StartDate)
+            .WithMessage("End date must be later than start date"); // ✅ Zgodnie z wytycznymi 8.2
+    }
+}
